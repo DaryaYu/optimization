36bd441a18fb063bcb50231fe9b8ae33909791f2
DaryaYu <33330219+DaryaYu@users.noreply.github.com>
2026-07-09 20:32:49 +0000

    redundant files removed

--- data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-DATA_PATH = 'data/Test task (Operations Research Scientist) (1).xlsx'
-
-
-def load_data(sheet_name: str, data_path=DATA_PATH):
-    return pd.read_excel(data_path, sheet_name=sheet_name, header=0)
